14f0fbc6aa17de1b3bf1e7701af066ae34a4972f
The Painting Ledger <mwashburn@gmail.com>
2026-06-01 15:51:17 -0600

    Add Metro expo-av dependency preflight

--- metro.config.js
+++ metro.config.js
@@ -1,4 +1,7 @@
 const { getDefaultConfig } = require('expo/metro-config');
+const { checkExpoAv } = require('./scripts/check-expo-av');
+
+checkExpoAv();
 
 const config = getDefaultConfig(__dirname);
 
